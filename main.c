0f113441cd4a3eef47a40150c967642c78032797
mauroamaroips <202000905@estudantes.ips.pt>
2023-03-08 19:24:02 +0000

    Final calculator

--- main.c
+++ main.c
@@ -2,10 +2,62 @@
 #include <stdlib.h>
 #include "input.h"
 
+int calculator(int,int,char);
+
 int main() {
 
-    /* Program code. */
-    printf("I do nothing at this moment.\n");
+    int n1, n2;
+    char op;
+
+    printf("Calculadora\n");
+    do
+    {
+        printf("Introduza o 1º número inteiro: ");
+
+    } while (!readInteger(&n1));
+
+    do
+    {
+            
+        printf("Introduza o 2º número inteiro: ");
+
+    } while (!readInteger(&n2));
+
+    do
+    {
+        printf("Introduza um operador aritmético (+, -, *, /): ");
+        scanf("%c", &op);
+        getchar(); //"consome" todos os caracteres que restarem no input buffer
+    } while (op != '+' && op != '-' && op != '*' && op != '/');
+
+    int result = calculator(n1,n2,op);
+    printf("Resultado da operação aritmética: %d\n", result);
 
     return EXIT_SUCCESS;
+}
+
+
+int calculator (int n1, int n2, char op){
+
+    int result = 0;
+    
+    switch (op)
+    {
+    case '+':
+        result = n1 + n2;
+        break;
+    case '-':
+        result = n1 - n2;
+        break;
+    case '*':
+        result = n1 * n2;
+        break;
+    case '/':
+        result = n1 / n2;
+        break;
+    default:
+        return 1;
+    }
+
+    return result;
 }
